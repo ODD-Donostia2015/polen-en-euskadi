77061ab16eb38d34681a4ba0cc51473df2a91a6d
AritzBi <aritzbi@gmail.com>
2015-02-20 11:08:35 +0100

    Web application's structure added

---
+++ polen_web/polen_web/polen_web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name polenWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the polenWebApp
+ */
+angular.module('polenWebApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ polen_web/polen_web/polen_web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name polenWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the polenWebApp
+ */
+angular.module('polenWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
